a7dfc0ca0117580b6deb21559c242652286bf289
Eugene <onufriev39@gmail.com>
2021-02-13 00:54:34 +0300

    Экран с регистрацией

--- app/src/main/java/com/example/softskills/MainActivity.kt
+++ app/src/main/java/com/example/softskills/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.softskills
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<MaterialButton>(R.id.loginBtn).setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    LoginActivity::class.java
+                )
+            )
+        }
     }
 }
