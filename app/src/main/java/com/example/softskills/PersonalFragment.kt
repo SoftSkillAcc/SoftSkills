0c22b61e8f9e06d1e22981bb60344c3e462b9e1c
bahu4 <bahu4@mail.ru>
2021-02-21 01:25:27 +0300

    добавил навигацию и устранил замечания

---
+++ app/src/main/java/com/example/softskills/PersonalFragment.kt
@@ -0,0 +1,7 @@
+package com.example.softskills
+
+import androidx.fragment.app.Fragment
+
+class PersonalFragment : Fragment(R.layout.fragment_personal) {
+
+}
---
+++ app/src/main/java/com/example/softskills/TrackingFragment.kt
@@ -0,0 +1,7 @@
+package com.example.softskills
+
+import androidx.fragment.app.Fragment
+
+class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+
+}
