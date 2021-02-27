340b5b565706cac9124076119ddf8924b4898810
bahu4 <bahu4@mail.ru>
2021-02-28 01:19:30 +0300

    добавил drawable ресурсы для индикации выбранного таба, + OnboardingActivity.kt и ее макет

---
+++ app/src/main/java/com/example/softskills/ui/activity/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.softskills.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.softskills.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
